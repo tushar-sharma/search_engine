0a2972ce3724d979c057d99f99d6850bbe073711
Tushar Sharma <tushar.sharma1729@gmail.com>
2017-12-23 23:33:46 +0000

    remove clutter

--- src/ex1.py
+++ src/ex1.py
@@ -13,19 +13,6 @@ def get_page(page):
 
     return page_source
 
-# extract href in html
-def get_next_target(page):
-    start_link = page.find('<a href=')
-
-    if start_link == -1:
-        return None, 0
-
-    start_quote = page.find('"', start_link)
-    end_quote = page.find('"', start_quote + 1)
-
-    url = page[start_quote + 1:end_quote]
-    return url, end_quote
-
 # check if relative url or absolute
 def is_absolute(link):
     return bool(urlparse(link).netloc)
@@ -41,14 +28,10 @@ def print_all_links(page, seed_page):
     soup = BeautifulSoup(page, "lxml", parse_only=SoupStrainer('a', href=True))
 
     for link in soup.find_all("a", href=True):
-
         if not is_absolute(link['href']):
             print (to_absolute(seed_page, link['href']))
-
         else:
             print (link['href'])
-
-        
 
 print_all_links(get_page(seed_page), seed_page)
 
