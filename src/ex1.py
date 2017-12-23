e3fdb1e74d734206011fd9e1f046712828db864f
Tushar Sharma <tushar.sharma1729@gmail.com>
2017-12-23 22:39:33 +0000

    extract links added

--- src/ex1.py
+++ src/ex1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import urllib2
+from bs4 import BeautifulSoup, SoupStrainer
 
 # get page get the source code and return string
 def get_page(page):
@@ -7,3 +8,29 @@ def get_page(page):
     page_source = response.read()
 
     return page_source
+
+# extract href in html
+def get_next_target(page):
+    start_link = page.find('<a href=')
+
+    if start_link == -1:
+        return None, 0
+
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+# print all links in the page
+# TODO: convert relative url to absolute
+def print_all_links(page):
+
+    soup = BeautifulSoup(page, "lxml", parse_only=SoupStrainer('a', href=True))
+
+    for link in soup.find_all("a", href=True):
+        print (link['href'])
+
+
+print_all_links(get_page('http://randomwits.com'))
+
